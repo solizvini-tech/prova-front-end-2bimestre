4076428fa29261454139f066b672c41273e334e7
solizvini-tech <solizvini@gmail.com>
2026-06-18 11:50:36 -0300

    senha

--- cadastro2.js
+++ cadastro2.js
@@ -1,4 +1,21 @@
+
+
 document.getElementById("btnvoltar").addEventListener("click", () => {
     window.location.href = "index.html";
 
 });
+
+function conferirSenhas() {
+
+    const senha = document.querySelector('#senha');
+    const confirmar = document.querySelector('#confirmar');
+    
+    
+    if (senha.value !== confirmar.value) {
+      mensagemErro.style.display = "block";
+      confirmaSenha.setCustomValidity("As senhas não conferem!");
+    } else {
+      mensagemErro.style.display = "none";
+      confirmar.setCustomValidity(""); 
+    }
+}
